f31e26486b987b80b69f7bd0f121e9b11f3a7fa1
qzheng <ellabook@yilakanshu.local>
2019-09-02 16:21:59 +0800

    modify main

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -9,6 +9,30 @@
 #include <cstdio>
 #include <cstdlib>
 
+int main(int argc, char *argv[])
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window=glfwCreateWindow(800,600,"LearnOpenGL",NULL,NULL);
+    if(window==NULL)
+    {
+        std::cout<<"Failed to create GLFW window"<<std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout<<"Failed to initialize GLAD"<<std::endl;
+        return -1;
+    }
+}
+/*
 int main(int argc, char * argv[]) {
 
     // Load GLFW and Create a Window
@@ -46,3 +70,4 @@ int main(int argc, char * argv[]) {
     }   glfwTerminate();
     return EXIT_SUCCESS;
 }
+*/
